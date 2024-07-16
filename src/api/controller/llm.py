bc8a366b18decf43e6329207314a7c4daa9e6a1b
prashun <prashun@monkeypatched-ai.com>
2024-07-16 14:30:37 +0530

    linting fix for llm controler

--- src/api/controller/llm.py
+++ src/api/controller/llm.py
@@ -1,20 +1,21 @@
+""" api contrtoller for the llm"""
+# pylint: disable=line-too-long,import-error,no-self-use,inconsistent-return-statements
+
+from fastapi.responses import JSONResponse
 from src.db.helpers.redis import RedisDB
 from src.utils.logger import logging as logger
 from src.api.vo.llm_request import LLMRequest
 from src.llm.model.sugriv import sugriv
-from fastapi.responses import JSONResponse
-from collections import defaultdict
 
 redis = RedisDB()
 sugriv = sugriv.get_model()
-
 
 def generate_top_k_results(request: LLMRequest):
     """generates the top k results using a greedy sampling methodology"""
     try:
         k_nearest = redis.get_top_k(request.prompt, int(request.top_k))
         k_nearest = []
-        if k_nearest != []:
+        if k_nearest:
             logger.debug("got the completions from redis cache")
             results = k_nearest
         else:
@@ -32,7 +33,7 @@ def generate_top_k_results(request: LLMRequest):
             merged_dict = {k: v for d in response for k, v in d.items()}
 
         return JSONResponse(content={"completion": merged_dict}, status_code=200)
-    except RuntimeError as e:
+    except RuntimeError as error:
         logger.error({"error": "run time error occured"})
-        logger.error(e)
-        return {"error": "run time error occured"}
+        logger.error(error)
+        return JSONResponse(content={"error": error}, status_code=500)
