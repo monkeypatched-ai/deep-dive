45a3a9e79ee2a43ee08f9337336cf689e24a56cf
prashun <prashun@monkeypatched-ai.com>
2024-07-16 15:43:02 +0530

    linting fix for dao

--- src/api/dao/graph.py
+++ src/api/dao/graph.py
@@ -1,18 +1,23 @@
+"""  this class is used to create the nodes for the graph """
+# pylint: disable=line-too-long,import-error,no-self-use,inconsistent-return-statements,abstract-method
+
 from neomodel import StructuredNode, StringProperty, UniqueIdProperty
 from src.db.helpers.graph import GraphDB
 
 graph = GraphDB()
 
-
 class DocumentChunks(StructuredNode):
+    """ create the chunk nodes """
     uid = UniqueIdProperty()
     data = StringProperty(unique_index=True)
 
     def to_json(self):
+        """ convert the result to json"""
         return {"uid": self.uid, "data": self.data}
 
 
 class Machines(StructuredNode):
+    """ create a node for the machine metadata """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     type = StringProperty(unique_index=True, required=True)
@@ -21,12 +26,14 @@ class Machines(StructuredNode):
 
 
 class Documents(StructuredNode):
+    """ create a node for the documents """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
     chunks = graph.create_relationship_to(DocumentChunks, "HAS_MANY")
 
     def to_json(self):
+        """ covert the document  metadata to json"""
         return {
             "uid": self.uid,
             "test": self.name,
@@ -36,6 +43,7 @@ class Documents(StructuredNode):
 
 
 class Prompt(StructuredNode):
+    """ create a node for the prompts """
     uid = UniqueIdProperty()
     text = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
@@ -43,6 +51,7 @@ class Prompt(StructuredNode):
 
 
 class SubProcess(StructuredNode):
+    """ create a node for the subprocess """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
@@ -50,6 +59,7 @@ class SubProcess(StructuredNode):
 
 
 class Process(StructuredNode):
+    """ create a node for the process  """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
@@ -58,6 +68,7 @@ class Process(StructuredNode):
 
 
 class Subject(StructuredNode):
+    """ create a node for the subjects """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
@@ -65,6 +76,7 @@ class Subject(StructuredNode):
 
 
 class Topic(StructuredNode):
+    """ create a node for the topics """
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
