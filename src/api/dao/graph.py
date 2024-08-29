884af1259a80e5df7bbe53ec819d46a6d214e53a
monkeypatched-ai <prashunshrikanthaveri@gmail.com>
2024-08-29 23:12:56 +0530

    Update graph.py
    remove unused dao

--- src/api/dao/graph.py
+++ src/api/dao/graph.py
@@ -16,15 +16,6 @@ class DocumentChunks(StructuredNode):
         return {"uid": self.uid, "data": self.data}
 
 
-class Machines(StructuredNode):
-    """ create a node for the machine metadata """
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True, required=True)
-    type = StringProperty(unique_index=True, required=True)
-    manufacturer = StringProperty(unique_index=True, required=True)
-    description = StringProperty(unique_index=True)
-
-
 class Documents(StructuredNode):
     """ create a node for the documents """
     uid = UniqueIdProperty()
@@ -40,48 +31,13 @@ class Documents(StructuredNode):
             "description": self.description,
             "chunks": self.chunks,
         }
-
-
+        
 class Prompt(StructuredNode):
     """ create a node for the prompts """
     uid = UniqueIdProperty()
     text = StringProperty(unique_index=True, required=True)
     description = StringProperty(unique_index=True)
     document = graph.create_relationship_to(Documents, "HAS_MANY")
-
-
-class SubProcess(StructuredNode):
-    """ create a node for the subprocess """
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True, required=True)
-    description = StringProperty(unique_index=True)
-    document = graph.create_relationship_to(Documents, "HAS_A")
-
-
-class Process(StructuredNode):
-    """ create a node for the process  """
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True, required=True)
-    description = StringProperty(unique_index=True)
-    subprocess = graph.create_relationship_to(SubProcess, "HAS_A")
-    machine = graph.create_relationship_to(Machines, "HAS_A")
-
-
-class Subject(StructuredNode):
-    """ create a node for the subjects """
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True, required=True)
-    description = StringProperty(unique_index=True)
-    process = graph.create_relationship_to(Process, "HAS_A")
-
-
-class Topic(StructuredNode):
-    """ create a node for the topics """
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True, required=True)
-    description = StringProperty(unique_index=True)
-    subject = graph.create_relationship_to(Subject, "HAS_A")
-
 
 class Generic(StructuredNode):
     uid = UniqueIdProperty()
